e21f4707579a5cceb7e2df1444f0b8a04530ff06
jks2185 <43832877+jks2185@users.noreply.github.com>
2023-04-20 16:09:02 -0400

    Add files via upload
    data cleaning

---
+++ data_cleaning.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+
+
+def get_data() -> pd.DataFrame:
+    path = r'C:\Users\e707088\Downloads'
+    file_name = 'Customer Sentiment Data.csv'
+    data = os.path.join(path, file_name)
+    return pd.read_csv(data)
+
+def rename_columns(df: pd.DataFrame, lst:dict) -> pd.DataFrame:
+    return df.rename(columns=lst)
+
+def filter_data(df:pd.DataFrame, lst:list) -> pd.DataFrame:
+    return df.filter(lst)
+
+def as_type(df:pd.DataFrame, type_string:str, column:str) -> pd.DataFrame:
+    return df.astype({column:type_string})
+
+def remove_value(df:pd.DataFrame, value:str, column:str) -> pd.DataFrame:
+    return df[df[column] != value]
+
+def main():
+    pass
+
+
+if '__name__' == '__main__':
+    main()
