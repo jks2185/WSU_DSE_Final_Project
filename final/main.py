80e2cb562edf9d5e4d00d91b3eef88f69bcd0402
jks2185 <43832877+jks2185@users.noreply.github.com>
2023-04-25 15:24:42 -0400

    final changes

--- final/main.py
+++ final/main.py
@@ -5,57 +5,22 @@ from dash import Input, Output
 from dash import dcc as dcc
 import pandas as pd
 import plotly.express as px
-import data_cleaning
-import plotly.graph_objects as go
 import numpy as np
-import ml_model
+from ml_model import get_split
 import pickle
 from sklearn.metrics import mean_squared_error
+import create_charts
+import data_cleaning
 
 # Load df dataset
 df = data_cleaning.main()
 df = data_cleaning.as_type(df, 'int', 'TOTAL_HOUSEHOLD_INCOME_-_CURRENT_DOLLARS')
 df = data_cleaning.as_type(df, 'int', 'AGE_OF_RESPONDENT')
-#create first figure
-df_group = df.groupby('AGE_OF_RESPONDENT')['TOTAL_HOUSEHOLD_INCOME_-_CURRENT_DOLLARS'].mean().reset_index()
-df_group = pd.DataFrame(df_group)
-df_group = df_group[['AGE_OF_RESPONDENT', 'TOTAL_HOUSEHOLD_INCOME_-_CURRENT_DOLLARS']].sort_values(by='AGE_OF_RESPONDENT',ascending=True)
-#new graph df
-df['SEX_OF_RESPONDENT'] = df['SEX_OF_RESPONDENT'].astype(str)
-#replace 1 with M and 2 with F
-df['SEX_OF_RESPONDENT'] = df['SEX_OF_RESPONDENT'].replace({'1': 'M', '2': 'F'})
-df.dropna(subset=['SEX_OF_RESPONDENT'], inplace=True)
-pivot = df.pivot_table(index=['SURVEY_YEAR'], columns='SEX_OF_RESPONDENT', values='TOTAL_HOUSEHOLD_INCOME_-_CURRENT_DOLLARS', aggfunc='mean')
-#reset the index
-pivot = pivot.reset_index()
-#drop rows where SEX is blank or not M or F
-df = df[df['SEX_OF_RESPONDENT'].isin(['M', 'F'])]
-#convert INCOME column to numeric type
-#create second figure
-df['PERSONAL_FINANCES_B/W_5_YEAR_AGO'] = df['PERSONAL_FINANCES_B/W_5_YEAR_AGO'].astype(str)
-#replace the financial condition number into str
-df['PERSONAL_FINANCES_B/W_5_YEAR_AGO'] = df['PERSONAL_FINANCES_B/W_5_YEAR_AGO'].replace({'1':'Better now','3' :'Same', '5': 'Worse now','8': 'DK','9':'NA'})
-df_2022 = df.query("SURVEY_YEAR == 2022")
-df_2022_PAGO5 = df_2022['PERSONAL_FINANCES_B/W_5_YEAR_AGO'].value_counts().reset_index().rename(columns={'index': 'sub_cat_values', 'PERSONAL_FINANCES_B/W_5_YEAR_AGO': 'counts'})
-df_2017 = df.query("SURVEY_YEAR == 2017")
-df_2017_PAGO = df_2017['PERSONAL_FINANCES_B/W_YEAR_AGO'].value_counts().reset_index().rename(columns={'index': 'sub_cat_values', 'PERSONAL_FINANCES_B/W_YEAR_AGO': 'counts'})
-#create next graph
-df_NewColumn = df[['PERSONAL_FINANCES_B/W_YEAR_AGO','VEHICLE_BUYING_ATTITUDES', 'ECONOMY_BETTER/WORSE_YEAR_AGO', 'UNEMPLOYMENT_MORE/LESS_NEXT_YEAR','ECONOMY_BETTER/WORSE_NEXT_YEAR','DURABLES_BUYING_ATTITUDES']]
-corr = df_NewColumn.corr()
-corr_columns = ['PRICES_UP/DOWN_NEXT_YEAR', 'DURABLES_BUYING_ATTITUDES', 'HOME_BUYING_ATTITUDES', 'VEHICLE_BUYING_ATTITUDES']
-corr_df = df[corr_columns]
-corr_matrix = corr_df.corr()
-annotations = np.round(corr_matrix.values, 2)
-counts = df['EDUCATION:_COLLEGE_GRADUATE'].value_counts()
-# Get the sizes and labels for the pie chart
-sizes = [counts[0], counts[1]]
-labels = ['Non-College Graduates', 'College Graduates']
-df_data = df.groupby('SURVEY_YEAR')[['INDEX_OF_CONSUMER_SENTIMENT', 'INDEX_OF_CURRENT_ECONOMIC_CONDITIONS', 'INDEX_OF_CONSUMER_EXPECTATIONS']].mean().reset_index()
-df_data = pd.DataFrame(df_data)
-df_data  = df_data[['SURVEY_YEAR', 'INDEX_OF_CONSUMER_SENTIMENT', 'INDEX_OF_CURRENT_ECONOMIC_CONDITIONS', 'INDEX_OF_CONSUMER_EXPECTATIONS']].sort_values(by='SURVEY_YEAR')
+
+fig1, fig2, fig3, fig4, fig5, fig6, fig7, fig8, fig9, fig10, fig11, fig12, fig13, fig14, fig15, fig17, fig18, fig19, fig20 = create_charts.main(df)
 
 #ml models creation
-X_train, X_test, y_train, y_test = ml_model.get_split(df)
+X_train, X_test, y_train, y_test = get_split(df)
 with open('linear_model.pkl', 'rb') as f:
     linear_model = pickle.load(f)
 # rf_model = pickle.load(open('rf_model.pkl', 'rb'))
@@ -70,67 +35,13 @@ xgb_pred = xgb_model.predict(X_test)
 linear_mse = mean_squared_error(y_test, linear_pred)
 rf_mse = mean_squared_error(y_test, rf_pred)
 xgb_mse = mean_squared_error(y_test, xgb_pred)
+ml_data = pd.DataFrame({'y_test' : y_test, 'prediction' : xgb_pred })
 
-#Graph creation
-fig1 = px.line(df_group, x='AGE_OF_RESPONDENT', y='TOTAL_HOUSEHOLD_INCOME_-_CURRENT_DOLLARS')
-#fig2 = px.bar(df_fin, x='AGE_OF_RESPONDENT', y=['PERSONAL_FINANCES_B/W_NEXT_YEAR', 'PERSONAL_FINANCES_B/W_YEAR_AGO'])
-fig2 = px.pie(df_2022_PAGO5, values='counts', names='sub_cat_values',
-        labels={'sub_cat_values': 'Financial Condition'},
-        #labels={'ou': 'ere'},
-        color_discrete_sequence=px.colors.qualitative.Plotly)
-fig2.update_traces(textposition='inside', textinfo='percent+label')
-fig2.update_layout(font=dict(size=18),
-                showlegend=True)
-fig3 = px.pie(df_2017_PAGO, values='counts', names='sub_cat_values',
-        labels={'sub_cat_values': 'Financial Condition'},
-        color_discrete_sequence=px.colors.qualitative.Plotly)
-fig3.update_traces(textposition='inside', textinfo='percent+label')
-fig3.update_layout(font=dict(size=18),
-                showlegend=True)
-fig4 = px.imshow(corr, x=corr.columns, y=corr.columns,
-        color_continuous_scale='RdBu', zmin=-1, zmax=1, aspect='auto')
-fig4.update_layout(
-                title={
-                    'text': 'Correlation Heatmap',
-                    'y': 0.95,
-                    'x': 0.5,
-                    'xanchor': 'center',
-                    'yanchor': 'top'
-                }
-            )
-#create double bar graph using plotly express
-fig5 = px.bar(pivot, x='SURVEY_YEAR', y=['M', 'F'], barmode='group')
-#set plot title and labels
-fig5.update_layout(title='Year on Year Average Income by Sex and Category', xaxis_title='Year, Sex', yaxis_title='Average Income')
-#figure 6
-fig6 = px.imshow(corr_matrix.values,
-        x=corr_columns, y=corr_columns,
-        color_continuous_scale='blues',
-        zmin=-1, zmax=1,
-        labels=dict(x='', y=''))
-# add annotations to the heatmap
-for i in range(len(corr_columns)):
-    for j in range(len(corr_columns)):
-        fig6.add_annotation(x=corr_columns[i], y=corr_columns[j],
-                        text=str(annotations[i][j]),
-                        showarrow=False, font=dict(color='white'))
-# update layout to show the color scale and adjust the margins
-fig6.update_layout(title="Correlation Matrix",
-                coloraxis_colorbar=dict(title='Correlation'),
-                margin=dict(l=100, r=100, t=50, b=100))
-#figure 7 pie chart
-fig7 = px.pie(values=sizes, names=labels, title='Proportion of College Graduates in Respondents')
-fig7.update_traces(textposition='inside', textinfo='percent+label')
-#willis graphs
-fig8 = px.line(df_data, x='SURVEY_YEAR', y=['INDEX_OF_CURRENT_ECONOMIC_CONDITIONS', 'INDEX_OF_CONSUMER_SENTIMENT'])
-fig9 = px.histogram(df, x='AGE_OF_RESPONDENT', nbins=20)
-fig10 = px.bar(df, x='MARITAL_STATUS_OF_RESPONDENT', y='TOTAL_HOUSEHOLD_INCOME_-_CURRENT_DOLLARS')
-fig11 = px.box(df, x='EDUCATION_OF_RESPONDENT', y='INDEX_OF_CONSUMER_SENTIMENT')
-fig12 = px.line(df_data, x='SURVEY_YEAR', y='INDEX_OF_CONSUMER_EXPECTATIONS')
-fig13 = px.histogram(df, x='INDEX_OF_CONSUMER_EXPECTATIONS', nbins=20)
-fig14 = px.box(df, x='REGION_OF_RESIDENCE', y='INDEX_OF_CURRENT_ECONOMIC_CONDITIONS')
-fig15 = px.scatter_matrix(df_data, dimensions=['INDEX_OF_CONSUMER_EXPECTATIONS', 'INDEX_OF_CONSUMER_SENTIMENT'])
-
+#Create ml models
+fig16 = px.scatter(ml_data, x='y_test', y='prediction', trendline='ols')
+fig16.update_traces(marker=dict(color='green'))
+fig16.update_xaxes(title='y test')
+fig16.update_yaxes(title='Prediction')
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
@@ -151,10 +62,18 @@ groupby_options = {
 
 @app.callback(
     Output('output', 'children'),
-    Input('input-1', 'value')
+    [Input(component_id='submit-button', component_property='n_clicks'),
+    Input('input-1', 'value')]
 )
-def update_output(value):
-    return f'Prediction: {value}'
+def update_output(n_clicks, value):
+
+    if int(n_clicks) > 0:
+        value1, value2 = value.split(',')
+        value1 = int(value1)
+        value2 = int(value2)
+        features = np.array([[value1, value2]])
+        result = xgb_model.predict(features)
+        return f'Prediction: {result}'
 #n_clicks = 0
 #input_value = ''
 # Define function to create data table
@@ -226,13 +145,17 @@ def display_page(x, groupby, tab):
                 ]),
                 html.P('This is what we found across all years.'),
                 html.H5('Correlation between Inflation, home amount, vehile, unemployment, and business conditions'),
+                html.P('I would expect these to have positive correlations with each other, as unemployment, buying attitudes, and personal finance should reflect on how the economy is doing.'),
                 html.Div([
                     dcc.Graph(id='fig4', figure=fig4)
                 ]),
+                html.P('This is more or less what we see, as personal finances being better or worse is affected by unemployment, and these all generally have a negative affect on how we view the economy.'),
                 html.H5('Correlation between Inflation and Buying Attitudes'),
+                html.P('We would think that buying attidues would be negatively related to inflation, as people would be less likely to buy things as price goes up.'),
                 html.Div([
                     dcc.Graph(id='fig6', figure=fig6)
                 ]),
+                html.P('This is what we see here, as they all have a negative correlation with each other.'),
                 html.H5('Personal Finace in 2017 v 2022'),
                 html.P('We thought there would be a drop-off in how people thought they were doing from 2017 to 2022.'),
                 html.Div(children=[
@@ -245,71 +168,129 @@ def display_page(x, groupby, tab):
     elif tab == 'graph_sentiment':
         layout = html.Div([
                 html.H5('Index of Consumer Sentiment V Current Economic Conditions'),
+                html.P('Here we would think sentiment would be in line with the economic conditions.'),
                 html.Div(
                     [
                         dcc.Graph(id='fig8', figure=fig8)
                     ]
                 ),
                 html.H5('Index of Consumer Sentiment'),
+                html.P('We were looking to see if there was anything that stood out in the frequency of good or bad responses.'),
                 html.Div(
                     [
                         dcc.Graph(id='fig13', figure=fig13)
                     ]
                 ),
+                html.P('The Bar graph shows a slightly higher amount of responses of "20" and "120" compared to others. \
+                       This could indicate a higher number of respondents answering due to successful economic expansion or in times of recession/depression.'),
                 html.H5('Consumer Sentiment by Education'),
+                html.P('We were interested to see if there was any relationships between education level and sentiment of respondents.'),
                 html.Div(
                     [
                         dcc.Graph(id='fig11', figure=fig11)
                     ]
                 ),
+                html.P('From this plot, we can see that there is a positive relationship between education level and consumer sentiment. \
+                       Respondents with higher education levels tend to have higher consumer sentiment scores, \
+                       as indicated by the higher median values for each level of education. \
+                       The range of consumer sentiment scores is also wider for respondents with higher education levels, indicating greater variability in the responses.'),
                 html.H5('Consumer Sentiment Over Time'),
+                html.P('This line graph helps to show how consumer sentiment tracked over the years since the survey began.'),
                 html.Div(
                     [
                         dcc.Graph(id='fig12', figure=fig12)
                     ]
                 ),
+                html.P('The graph illustrates the higher amount of responses of "20" and "120" compared to others from the previous bar chart may be associated with specific events in the economy. \
+                       All of the drops in sentiment coincide with recessions over the past 40 years (1980, early 90s, 2001, 2008, 2020).'),
                 html.H5('Current Economic Conditions by Region'),
+                html.P('We were interested to see if there was any relationships between Consumer expectations and sentiment across regions.'),
                 html.Div(
                     [
                         dcc.Graph(id='fig14', figure=fig14)
                     ]
                 ),
-                html.H5('Current Economic Conditions by Region'),
+                html.P('The matrix above shows that regions 1 and 4 are more tightly correlated while 2 and 3 had more variance in their answers. This may indicate greater economic wealth gaps in those regions or different socioeconomic situations.'),
+                html.H5('Consumer Expectations V Sentiment'),
+                html.P('We were curious to see if there was any variance in the economic conditions of those surveyed.'),
                 html.Div(
                     [
                         dcc.Graph(id='fig15', figure=fig15)
                     ]
                 ),
+                html.P('The histogram shows a roughly normal distribution indicating there is not a significant difference across regions.'),
             ])
         return layout
 
     elif tab == 'graph_demographics':
         layout = html.Div([
                 html.H5('Proportion of College Grads in Respondents'),
+                html.P('We would think this would lean towards more non-college graduates.'),
                 html.Div([
                     dcc.Graph(id='fig7', figure=fig7)
                 ]),
+                html.P('This is what we see here, as there are more respondents without degrees than with.'),
                 html.H5('Age of Respondent Distribution'),
+                html.P('We would think this would be somewhat evenely distributed across all ages.'),
                 html.Div([
                     dcc.Graph(id='fig9', figure=fig9)
                 ]),
-                html.H5('Education of Respondent v Consumer Sentiment'),
+                html.P('This leans more towards then working ages between 20s and 60 year old adults.'),
+                html.H5('Education of Respondent v Current Economic Conditions'),
+                html.P('We would think that those with a higher education would have a higher view on the economy with more money.'),
                 html.Div([
                     dcc.Graph(id='fig11', figure=fig11)
+                ]),
+                html.P('We see that this is the case, with those with a lower education having a lower view.'),
+                html.H5('Home Buying Attitudes by Sex'),
+                html.P('Here we would think that men would have a higher view on buying a house.'),
+                html.Div([
+                    dcc.Graph(id='fig17', figure=fig17)
+                ]),
+                html.P('This is actually not the case, with women having a higher view on it throughout the years.'),
+                html.H5('Car Buying Attitudes by Sex'),
+                html.P('Here we would think that men would have a higher view on buying a car.'),
+                html.Div([
+                    dcc.Graph(id='fig18', figure=fig18)
+                ]),
+                html.P('This is actually not the case, with women having a higher view on it throughout the years.'),
+                html.H5('Home Buying Attitudes by Marital Status'),
+                html.P('We would think that those who are married would have the highest sentiment towards buying a house.'),
+                html.Div([
+                    dcc.Graph(id='fig19', figure=fig19)
+                ]),
+                html.P('This is not the case, as we see that those who are separated do.'),
+                html.H5('Vehicle Buying Attitudes by Marital Status'),
+                html.P('Here would think that those who are married would have a higher need for a car and thus have a higher buying attitude.'),
+                html.Div([
+                    dcc.Graph(id='fig20', figure=fig20)
+                ]),
+                html.P('Although it fluctuates through the years, separated has the highest some years, followd by widowed, and never married.'),
                 ])
-                ])
+                
         return layout
 
     elif tab == 'ml':
         layout = html.Div([
                 dcc.Input(id='input-1', type='text', value=''),
+                html.Button('Submit', id='submit-button', n_clicks=0),
                 html.Br(),
+                html.P('Output:'),
                 html.Div(id='output'),
                 
                 html.H2('Mean Square Error of Models:'),
                 html.P(f'Linear MSE: {linear_mse}'),
                 html.P(f'RF MSE: {rf_mse}'),
-                html.P(f'RF MSE: {xgb_mse}')
+                html.P(f'XGB MSE: {xgb_mse}'),
+                html.P('We can see here that the XgBoost model has the lowest mean squared error, so that is the model we have used for this prediction.'),
+                html.H5('Residuals'),
+                html.Div(
+                    [
+                        dcc.Graph(id='fig16', figure=fig16)
+                    ]
+                ),
+                html.P('Here we can see that there isnt much of a pattern for the model to follow as far as making predictions accurately. In the future, I would use more dimensions for this model since only having a couple did not create \
+                       a viable model.'),
                 ])
         return layout
 
